b0a9e094800c57aa64da77b2c9b432ccb0fd83bd
4lex4c <44lex.dev@gmail.com>
2024-06-13 22:55:52 -0300

    Modificación en el archivo Index del Bot

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// Importa la biblioteca de Discord.js y el Token del Bot
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const token = require("./config.json");
+// Importación de la Biblioteca de Discord.js y Módulos de Node
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
 
 // Crea una nueva Instancia a partir de la clase Client
 const client = new Client({ intents: [GatewayIntentBits.Guilds]});
+
+client.commands = new Collection();
 
 // Cuando el cliente (bot) esté listo, se ejecuta este código sólo una vez (once).
 // Imprime en la consola un mensaje avisando que el cliente está listo para operar y conectado correctamente a Discord.

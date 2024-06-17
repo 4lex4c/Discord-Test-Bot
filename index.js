b8a4795a6198f478e3d514b25c01bfb748db395c
4lex4c <44lex.dev@gmail.com>
2024-06-17 11:39:31 -0300

    Modificación en el Archivo Principal del Bot

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds]});
 
 client.commands = new Collection();
 
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[Advertencia] Al comando en ${filePath} le falta la propiedad 'data' o 'execute' requerida.`);
+        };
+    }
+}
+
 // Cuando el cliente (bot) esté listo, se ejecuta este código sólo una vez (once).
 // Imprime en la consola un mensaje avisando que el cliente está listo para operar y conectado correctamente a Discord.
 client.once(Events.ClientReady, readyClient => {
